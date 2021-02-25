1f3257990f7edc86040b4d529d4b3bb74b0170d7
Cédric B <cedricbardaine@gmail.com>
2021-02-25 22:03:00 +0100

    add primarySwatch

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hearts Sender',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,21 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+
+        // 0xFF669C5E
+        primarySwatch: const MaterialColor(0xFF669C5E, const <int, Color>{
+          50: const Color(0xFF669C5E),
+          100: const Color(0xFF669C5E),
+          200: const Color(0xFF669C5E),
+          300: const Color(0xFF669C5E),
+          400: const Color(0xFF669C5E),
+          500: const Color(0xFF669C5E),
+          600: const Color(0xFF669C5E),
+          700: const Color(0xFF669C5E),
+          800: const Color(0xFF669C5E),
+          900: const Color(0xFF669C5E),
+        }),
+
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
