f0432704a055559513a7379a35bdf680f468b116
Cédric B <cedricbardaine@gmail.com>
2021-02-27 20:21:17 +0100

    add stats container

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -4,28 +4,58 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // Container(height: 32.0),
-        TextButton(
-          onPressed: () {
-            print("ouille");
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith<Color>(
-              (Set<MaterialState> states) {
-                if (states.contains(MaterialState.pressed))
-                  return Theme.of(context).colorScheme.secondary;
-                return Theme.of(context)
-                    .colorScheme
-                    .primary; // Use the component's default.
-              },
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Container(height: 32.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    print("ouille");
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed))
+                          return Theme.of(context).colorScheme.secondary;
+                        return Theme.of(context)
+                            .colorScheme
+                            .primary; // Use the component's default.
+                      },
+                    ),
+                  ),
+                  child: Text("Send a heart ❤",
+                      style: TextStyle(color: Colors.white)),
+                )
+              ],
             ),
-          ),
-          child: Text("Send a heart", style: TextStyle(color: Colors.white)),
+            Container(
+              height: 32.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("My hearts")],
+            ),
+            Container(height: 8.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Nombre de ❤ total : " + "###"),
+                Text("Nombre de ❤ cette semaine : " + "###"),
+                Text("Nombre de ❤ aujourd'hui : " + "###"),
+              ],
+            ),
+            Container(
+              height: 8.0,
+            ),
+            // TODO: add ❤ chart
+          ],
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
